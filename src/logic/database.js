59e2900302bba5be7f336c5d9974dadfc09d190d
Carlos <carlosoftwar@gmail.com>
2021-03-18 03:08:32 -0300

    Create : random auxiliary names

---
+++ src/logic/database.js
@@ -0,0 +1,17 @@
+var names = [
+    "tabse",
+    "ovesa",
+    "brice",
+    "ucins",
+    "iders",
+    "cockp",
+    "demph",
+    "iandi",
+    "ithem",
+    "bland",
+    "corph", 
+    "erode",
+    "bleil",
+    "emido",
+    "swana"
+];
